68f5a5cdc5626b5d1cfd9ba8f5f23e04244389f9
lumenthi <lumenthi@student.42.fr>
2022-11-23 15:56:36 +0100

    :wrench: Change rights for k3s install

--- p1/scripts/worker-node.sh
+++ p1/scripts/worker-node.sh
@@ -6,7 +6,7 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 echo "Installing k3s..."
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
 
 # echo "Starting server..."
 # sudo /usr/local/bin/k3s server
