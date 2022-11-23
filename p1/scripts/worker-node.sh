5bef6243c0ef64c2a3b024939e160a3a7dfb01fc
lumenthi <lumenthi@student.42.fr>
2022-11-23 15:40:04 +0100

    :sparkles: Add install scripts

--- p1/scripts/worker-node.sh
+++ p1/scripts/worker-node.sh
@@ -1,1 +1,17 @@
-echo "SALUT"
+echo "Configuring worker node"
+
+echo "Installing kubectl prerequisites..."
+cd /etc/yum.repos.d/
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
+echo "Installing k3s..."
+curl -sfL https://get.k3s.io | sh -
+
+# echo "Starting server..."
+# sudo /usr/local/bin/k3s server
+
+echo "Checking nodes..."
+kubectl get nodes
+
+echo "Worker configuration done"
