c6d41e077889c72eda9a6b671b5104e5db1d0bfd
lumenthi <lumenthi@student.42.fr>
2022-11-24 09:21:30 -0500

    :sparkles: Configuring nodes IP for p1

--- p1/scripts/master-node.sh
+++ p1/scripts/master-node.sh
@@ -6,7 +6,9 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 echo "Installing k3s..."
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip 192.168.42.110"\
+	K3S_TOKEN="713705"\
+	K3S_KUBECONFIG_MODE="644" sh -
 
 # sudo service k3s status
 # sudo /usr/local/bin/k3s server
--- p1/scripts/worker-node.sh
+++ p1/scripts/worker-node.sh
@@ -6,7 +6,10 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 echo "Installing k3s..."
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --node-ip 192.168.42.111"\
+	K3S_URL="https://192.168.42.110:6443"\
+	K3S_TOKEN="713705"\
+	K3S_KUBECONFIG_MODE="644" sh -
 
 # sudo service k3s status
 # sudo /usr/local/bin/k3s server
