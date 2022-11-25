fe260d617cd9a254d8c4f454c266328fe140ecb2
Lucas Nicosia <lucas@lucas-debian11>
2022-11-25 18:34:29 +0100

    p3 wip.

---
+++ p3/install.sh
@@ -0,0 +1,45 @@
+
+# ===============Docker installation===============
+
+# Dependencies
+sudo apt update
+sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+# Import Docker GPG key
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg
+
+# Add Docker repo to debian
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+# Docker install
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+# Start service
+sudo systemctl enable --now docker
+
+# Docker group
+sudo usermod -aG docker $USER
+newgrp docker
+exit
+
+# Check if sucessfully installed
+docker version
+sudo groupadd docker
+sudo usermod -aG $USER
+
+# ===============kubectl installation===============
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+# Output must be "kubectl: Ok"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm -f kubectl
+
+# ===============k3d installation===============
+
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
