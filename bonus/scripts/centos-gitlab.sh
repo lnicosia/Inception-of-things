376148d6fdc8862c3b7552008b0ebf4afbf15b45
lumenthi <lumenthi@student.42.fr>
2022-11-29 05:08:39 -0500

    :sparkles: First version of bonus

---
+++ bonus/scripts/centos-gitlab.sh
@@ -0,0 +1,27 @@
+# Yum update
+sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+# sudo yum update -y
+
+# Gitlab dependancies
+sudo yum -y install curl vim policycoreutils python3-policycoreutils
+
+# Gitlab install
+curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+sudo yum install gitlab-ce -y
+
+# Gitlab config
+# Edit configs
+sudo vim /etc/gitlab/gitlab.rb
+sudo gitlab-ctl reconfigure
+
+# Get root's default password
+sudo cat /etc/gitlab/initial_root_password
+
+# Change grp visibility to allow public repos
+# Change group visibility
+#    On the top bar, select Menu > Groups and find your project.
+#    On the left sidebar, select Settings > General .
+#    Expand Naming, visibility .
+#    Under Visibility level select either Private , Internal , or Public .
+#    Select Save changes .
